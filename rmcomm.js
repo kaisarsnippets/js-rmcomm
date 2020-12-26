ca9ee9a5e71d59ad921b34f905f2a48394f31d77
KaisarCode <kaisar@kaisarcode.com>
2020-12-26 01:28:45 -0300

    ommit regex double bars

--- rmcomm.js
+++ rmcomm.js
@@ -1,11 +1,12 @@
 // Remove comments
 function rmcomm(str, remove_html_comments) {
     remove_html_comments = remove_html_comments || false;
+    str = str.replace(/\\\/\//g, '__KCRMCOMMREGEXBARS__'); 
     var rx = /\/\*[\s\S]*?\*\/|(?<!:)\/\/.*$/gm;
     str = str.replace(rx, '');
     if (remove_html_comments) {
         rx = /<!--[\s\S]*?-->/gm;
         str = str.replace(rx, '');
-    }
+    } str = str.replace(/__KCRMCOMMREGEXBARS__/g, '\\//');
     return str;
 }
